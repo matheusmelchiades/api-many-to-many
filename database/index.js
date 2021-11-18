7a6b75804216acd12af69a9edc475e924ace24ce
Matheus Maciel <matheusmacielmelchiades@hotmail.com>
2021-11-18 14:09:17 -0300

    add insert and update index in database mock

--- database/index.js
+++ database/index.js
@@ -3,9 +3,27 @@ const mock = require('./mock.json')
 
 const db = new alasql.Database()
 
+db.getNextIndex = async (tableName) => {
+    const data = await db.exec(`SELECT id FROM ${tableName}`)
+    const next = getLastId(data);
+    
+    return next + 1
+}
+
+function getLastId(data) {
+
+    return data
+        .map(row => row.id)
+        .sort((a, b) => a - b)
+        .pop()
+}
+
 Object
     .entries(mock)
     .forEach(([tableName, data]) => {
+        const indice = getLastId(data);
+
+        db.indices[tableName] = indice
 
         if (!db.tables[tableName])
             db.tables[tableName] = { data }
